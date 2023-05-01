24a3508158cf8091300d5999d832dcc21063b5ae
Adinland <116732726+Adinland@users.noreply.github.com>
2023-05-02 00:04:36 +0700

    Add files via upload

---
+++ lesson8.py
@@ -0,0 +1,38 @@
+print("\tЗадание 1\nВведите размер, затем содержимое списка")
+len = int(input())
+arr = []
+for i in range(len):
+    arr.append(int(input()))
+arr.reverse()
+print(arr)
+
+print("\tЗадание 2\nВведите размер, затем содержимое списка")
+len = int(input())
+arr = []
+for i in range(len):
+    arr.append(int(input()))
+arr = arr[-1:] + arr[:-1]
+print(arr)
+
+
+print("\tЗадание 3\nВведите грузоподьёмность лодки, количество людей, затем вес каждого")
+b_weight = int(input())
+amount_people = int(input())
+p_weight = []
+for i in range(amount_people):
+    p_weight.append(int(input()))
+"""
+тестовые данные
+b_weight = 120
+amount_people = 5
+p_weight = [50, 60, 65, 100, 50]
+3 лодки
+"""
+all_weight = 0
+amount_boat = 0
+for i in p_weight:
+    all_weight += i
+while all_weight > 0:
+    amount_boat += 1
+    all_weight -= b_weight
+print(f"Минимально необходимое количество лодок: {amount_boat}")
