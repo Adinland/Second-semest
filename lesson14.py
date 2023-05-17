b09777a4b6c07597da7aca0a2a33e725fefe9331
Adinland <116732726+Adinland@users.noreply.github.com>
2023-05-17 14:56:28 +0700

    Update lesson14.py

--- lesson14.py
+++ lesson14.py
@@ -2,6 +2,8 @@ def list_out(my_list, num):
     if num < len(my_list):
         print(my_list[num], end=' ')
         list_out(my_list, num + 1)
+    else:
+        print("\nКонец списка")
 
 my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 list_out(my_list, 1)
